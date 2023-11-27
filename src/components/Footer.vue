397bdd5bac62f88b619782b52868bc5e3c2529bf
Allen <lcf7891@gmail.com>
2023-11-27 22:08:23 +0800

    加入 footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const isYear = ref(new Date().getFullYear())
+</script>
+
+<template>
+  <footer class="text-center bg-quaternary py-5">
+    <p class="text-sm text-white">台灣走走ㆍTai Walk &copy; {{ isYear }}</p>
+  </footer>
+</template>
