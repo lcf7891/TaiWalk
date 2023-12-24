b733d6ea45f8bfa2c455556752e339ff7dbad680
Allen <lcf7891@gmail.com>
2023-12-24 15:39:38 +0800

    重新命名頁碼 css

--- src/components/PageNavigation.vue
+++ src/components/PageNavigation.vue
@@ -4,13 +4,13 @@
 
 <template>
   <section class="md:pt-20 md:mb-28 py-16">
-    <ul class="page-nav">
-      <li class="page-arrow page-prev page-disabled"></li>
-      <li class="mx-1 active">1</li>
-      <li class="mx-1">2</li>
-      <li class="mx-1">3</li>
-      <li class="mx-1">...</li>
-      <li class="page-arrow page-next"></li>
+    <ul class="pagination">
+      <li class="page-item page-arrow page-prev page-disabled"></li>
+      <li class="page-item active mx-1">1</li>
+      <li class="page-item mx-1">2</li>
+      <li class="page-item mx-1">3</li>
+      <li class="page-item mx-1">...</li>
+      <li class="page-item page-arrow page-next"></li>
     </ul>
   </section>
 </template>
